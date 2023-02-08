4096bde8a69dde5cafec48fe1478aee65f807ecc
“BarrioTropical” <barrio>
2023-02-08 17:46:36 +0100

    carousel brand component
    Signed-off-by: “BarrioTropical” <“barrio>

---
+++ src/components/CarouselBrandComponent.vue
@@ -0,0 +1,117 @@
+<template>
+    <h2 class="text-center mt-4">Choose between our partner brand</h2>
+    <carousel :itemsToShow="5" :wrapAround="true" :transition="1000" :autoplay="1500">
+        <slide v-for="(slide, index) in homePaths" :key="index">
+            <div class="mt-2">
+                <img class="carousel__item" :src="slide.path" :alt="slide.alt">
+                <h3 class="text-white pt-2 text-center">{{ slide.info }}<span class="dot">.</span></h3>
+            </div>
+        </slide>
+
+        <template #addons>
+            <navigation />
+        </template>
+    </carousel>
+</template>
+
+<script>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+
+export default {
+    name: 'CarouselBrandComponent',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    data: function () {
+        return {
+
+            homePaths: [
+                {
+                    path: 'img/jumbo-1.jpg',
+                    alt: 'moisturizer',
+                    info: 'cream',
+                    id: 1,
+                },
+                {
+                    path: 'img/jumbo-2.jpg',
+                    alt: 'makeup',
+                    info: 'makeup',
+                    id: 2,
+                },
+                {
+                    path: 'img/jumbo-3.jpg',
+                    alt: 'eyeshadow',
+                    info: 'brushes',
+                    id: 3,
+                },
+                {
+                    path: 'img/jumbo-4.jpg',
+                    alt: 'eyeliner',
+                    info: 'eyeliner',
+                    id: 4,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use './../assets/styles/partials/variables' as *;
+@import "../../node_modules/vue3-carousel/dist/carousel.css";
+
+
+.dot{
+    font-size: 30px;
+    
+}
+.carousel__slide {
+    padding: 5px;
+}
+
+.carousel__viewport {
+    perspective: 200px;
+}
+
+.carousel__track {
+    transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+    transition: 0.5s;
+}
+
+.carousel__slide {
+    opacity: 0.9;
+    transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+    transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: rotateY(0) scale(1.1);
+}
+
+img {
+    width: 200px;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,17 +4,20 @@
     </div>
     <div class="container mt-5 mb-5">
         <CarouselComponent></CarouselComponent>
+        <CarouselBrandComponent></CarouselBrandComponent>
     </div>
 </template>
 
 <script>
 import CarouselComponent from '../components/CarouselComponent.vue';
 import JumboComponent from '../components/JumboComponent.vue';
+import CarouselBrandComponent from '../components/CarouselBrandComponent.vue';
 export default {
     name: 'HomePage',
     components: {
         JumboComponent,
-        CarouselComponent
+        CarouselComponent,
+        CarouselBrandComponent
     }
 }
 </script>
