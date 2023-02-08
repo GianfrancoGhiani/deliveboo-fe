71ed7980fe25e5edc34ed155efd335045ef43697
mattia.rollo <ste.tia@hotmail.it>
2023-02-08 14:42:20 +0100

    import boostrap in main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { router } from './router';
 import App from './App.vue'
 
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './assets/styles/main.scss'
 
