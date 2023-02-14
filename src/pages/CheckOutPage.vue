a20c9d516d1b36664ebfcf7dcb3b56e25661c19a
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-02-14 13:47:23 +0100

    checkout adj

--- src/pages/CheckOutPage.vue
+++ src/pages/CheckOutPage.vue
@@ -1,14 +1,7 @@
 <template>
     <div id="checkout" class="d-flex flex-column align-items-center">
 
-        <!-- <PaymentComponent v-if="!braintreeLoading" :authorization="apiToken" @loading="handleLoading"
-            @onSuccess="paymentOnSuccess" @onError="paymentoOnError" ref="paymentRef"></PaymentComponent>
-        <div v-if="error">
-            {{ error }}
-            
-        </div>
-        <button :disabled="disableBuyButton" @click.prevent="beforeBuy">Procedi con l acquisto</button>
-         -->
+
         <div class="w-25">
             <div id="dropin-wrapper">
                 <div id="checkout-message"></div>
@@ -17,7 +10,7 @@
             </div>
         </div>
         <div id="payment-message" class="w-auto"></div>
-        <!-- <button id="submit-button" class="button button--small button--green">Purchase</button> -->
+
 
 
 
@@ -25,20 +18,17 @@
 </template>
 
 <script>
-import PaymentComponent from '../components/PaymentComponent.vue';
+
 import axios from 'axios';
 import { store } from '../store';
-// import { defineAsyncComponent } from 'vue';
-// const PaymentComponent = defineAsyncComponent(() => import('../components/PaymentComponent.vue'))
+
 export default {
     name: "CheckOutPage",
-    components: { PaymentComponent },
+
     data() {
         return {
             apiToken: '',
             message: '',
-            braintreeLoading: true,
-            disableBuyButton: true,
             dataProductsIds: [],
             form: {
                 token: '',
@@ -97,12 +87,12 @@ export default {
                                 message.innerHTML = res.data.message;
                                 button.innerHTML = 'Paid';
                             })
-                            // console.log(payload)
+
                         });
                     })
                 });
 
-                // this.braintreeLoading = false
+
 
             })
         },
