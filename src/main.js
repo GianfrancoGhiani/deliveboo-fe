38bf8c34f2105bae63bbd3fe62bc1373b0f61a56
mattia.rollo <ste.tia@hotmail.it>
2023-02-14 17:49:42 +0100

    fix main.js with braintree

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { router } from './router';
-import vuebraintree from 'vue-braintree';
+
 import App from './App.vue'
 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import './assets/styles/main.scss'
 
-createApp(App).use(vuebraintree).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
