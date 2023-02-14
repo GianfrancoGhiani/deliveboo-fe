48161a71a521c5c27091b1c942fbbc989f56266a
GianfrancoGhiani <gianfranco.ghiani.gg@gmail.com>
2023-02-14 13:46:14 +0100

    added ful payment and checkout styling

--- src/pages/CheckOutPage.vue
+++ src/pages/CheckOutPage.vue
@@ -1,18 +1,26 @@
 <template>
-    <div>
-        <!-- <suspense> -->
-            <!-- <template #default> -->
-                <PaymentComponent v-if="!braintreeLoading" :authorization="apiToken" @loading="handleLoading"
-                    @onSuccess="paymentOnSuccess" @onError="paymentoOnError" ref="paymentRef"></PaymentComponent>
-                <div v-if="error">
-                    {{ error }}
-                </div>
-            <!-- </template> -->
-            <!-- <template #fallback>
-                loading
-            </template> -->
-        <!-- </suspense> -->
+    <div id="checkout" class="d-flex flex-column align-items-center">
+
+        <!-- <PaymentComponent v-if="!braintreeLoading" :authorization="apiToken" @loading="handleLoading"
+            @onSuccess="paymentOnSuccess" @onError="paymentoOnError" ref="paymentRef"></PaymentComponent>
+        <div v-if="error">
+            {{ error }}
+            
+        </div>
         <button :disabled="disableBuyButton" @click.prevent="beforeBuy">Procedi con l acquisto</button>
+         -->
+        <div class="w-25">
+            <div id="dropin-wrapper">
+                <div id="checkout-message"></div>
+                <div id="dropin-container"></div>
+                <button id="submit-button" class="btn">Submit payment</button>
+            </div>
+        </div>
+        <div id="payment-message" class="w-auto"></div>
+        <!-- <button id="submit-button" class="button button--small button--green">Purchase</button> -->
+
+
+
     </div>
 </template>
 
@@ -28,8 +36,10 @@ export default {
     data() {
         return {
             apiToken: '',
+            message: '',
             braintreeLoading: true,
             disableBuyButton: true,
+            dataProductsIds: [],
             form: {
                 token: '',
                 products: [],
@@ -38,13 +48,10 @@ export default {
 
     },
     mounted() {
-        axios.get(`${store.apiBaseUrl}/order`).then((res) => {
-            console.log(res.data.token);
-            this.apiToken = res.data.token;
-            this.braintreeLoading = false
-        })
-        this.getIds();
-    },
+        this.getToken();
+
+    }
+    ,
     methods: {
         handleLoading() {
             this.disableBuyButton = false;
@@ -53,25 +60,51 @@ export default {
             this.form.token = nonce
             this.buy()
         },
-        getIds() {
-            const productsArray = JSON.parse(localStorage.getItem('cart'));
-            let productsIds = [];
-            productsArray.forEach(element => {
-                productsIds.push(element.id);
-            });
-            console.log(productsIds)
-        },
-        beforeBuy() {
-            this.$refs.paymentRef.$refs.paymentBtnRef.click
-        },
-        buy() {
-            this.disableBuyButton = true;
-            axios.post(`${store.apiBaseUrl}/order/payment`, { ...this.form }.then((response) => {
-                this.message = response.message
-                this.$router.push({ path: `/` })
-                localStorage.clear();
-            }))
-            // this.clearCart();
+        getToken() {
+            axios.get(`${store.apiBaseUrl}/order`).then((res) => {
+                this.apiToken = res.data.token;
+
+                var button = document.querySelector('#submit-button');
+
+                braintree.dropin.create({
+                    authorization: this.apiToken,
+                    selector: '#dropin-container'
+                }, function (err, instance) {
+                    button.addEventListener('click', function () {
+                        button.setAttribute("disabled", true);
+                        button.innerHTML = 'Loading...';
+                        instance.requestPaymentMethod(function (err, payload) {
+
+                            const productsArray = JSON.parse(localStorage.getItem('cart'));
+                            let productsIds = [];
+                            productsArray.forEach(element => {
+                                productsIds.push(element.id);
+                            });
+
+                            console.log(payload.nonce)
+                            axios.post(`${store.apiBaseUrl}/order/payment`,
+                                {
+                                    token: payload.nonce,
+                                    products: productsIds
+                                }
+                            ).then((res) => {
+                                const message = document.querySelector('#payment-message');
+                                if (res.data.success) {
+                                    message.classList.add('alert', 'alert-success', 'mb-3', 'mt-3');
+                                } else {
+                                    message.classList.add('alert', 'alert-danger', 'mb-3', 'mt-3');
+                                }
+                                message.innerHTML = res.data.message;
+                                button.innerHTML = 'Paid';
+                            })
+                            // console.log(payload)
+                        });
+                    })
+                });
+
+                // this.braintreeLoading = false
+
+            })
         },
         clearCart() {
             localStorage.clear();
@@ -83,5 +116,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@use '../assets/styles/partials/variables' as *;
 </style>
