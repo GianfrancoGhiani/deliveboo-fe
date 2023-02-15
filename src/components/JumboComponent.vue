1351274890d94f67bbecf30fdfc2481e77feacc9
mattia.rollo <ste.tia@hotmail.it>
2023-02-15 17:39:40 +0100

    style

--- src/components/JumboComponent.vue
+++ src/components/JumboComponent.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container-sm containerVideo">
-      <video src="public/video/production ID_4170500.mp4" autoplay muted loop class="w-100"></video>
+    <div class="container-fluid p-0 containerVideo">
+        <video src="public/video/production ID_4170500.mp4" autoplay muted loop height="400"></video>
     </div>
 
 
@@ -13,16 +13,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-img{
+img {
     width: 100%;
     height: 400px;
     object-fit: cover;
     position: top;
 }
-video{
+
+video {
     width: 100%;
-    object-fit: fill;
+    object-fit: cover;
 }
-
-
 </style>
