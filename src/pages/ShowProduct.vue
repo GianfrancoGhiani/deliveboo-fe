eb592216b378fc3bc3c5b3a9b6e084f486029891
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2023-02-17 09:56:36 +0100

    card fixed

--- src/pages/ShowProduct.vue
+++ src/pages/ShowProduct.vue
@@ -1,26 +1,19 @@
 <template>
-
-    <div class="container-sm">
-        <div class="py-5">
-            <div class="wrap d-flex">
-                <div class="tile">
-                    <img :src="`${store.imagBasePath}${product.image_url}`" />
-                </div>
-                <div class="text ms-3">
-                    <div class="color">
-                        <h2>{{ product.name }}</h2>
-                    </div>
-                    <p class="text-capitalize">{{ product.ingredients }}</p>
-                    <span>{{ product.price }}</span>
-                </div>
-            </div>
-        </div>
-    </div>
+    <ul class="card-wrapper mymargin d-flex justify-content-center">
+        <li class="card">
+            <img :src="`${store.imagBasePath}${product.image_url}`" alt=''>
+            <h5 class="card-title mt-2 text-center">{{ product.name }}</h5>
+            <h5 class="text-center ingredient">Ingredients:</h5>
+            <p class="card-text text-capitalize text-center">{{ product.ingredients }}</p>
+            <button class="btn btn-primary bg-orange w-100" @click="goBack()">Back</button>
+        </li>
+</ul>
 </template>
 
 <script>
 import axios from 'axios';
 import { store } from '../store';
+
 export default {
     name: 'ShowProduct',
     data() {
@@ -41,6 +34,9 @@ export default {
     created() {
         this.getProduct();
         console.log(this.$route.params)
+    },
+    goBack() {
+        window.history.back()
     }
 }
 </script>
@@ -48,12 +44,84 @@ export default {
 <style lang="scss" scoped>
 @use './../assets/styles/partials/variables' as *;
 
-.text {
-    width: 300px;
-    height: 200px;
+.card {
+    width: 400px;
+    background-color: #0F0F0F;
+
+    img {
+        border-radius: 0.5rem 0.5rem;
+        width: 100%;
+        object-fit: cover;
+        max-height: max(10rem, 30vh);
+        mix-blend-mode: var(--card-blend-mode);
+
+    }
+
+    > :last-child {
+        margin-bottom: 0;
+    }
+
+    &:hover,
+    &:focus-within {
+        --card-gradient: #24a9d5 max(8.5rem, 20vh);
+    }
 }
 
-.color {
+body {
+    display: grid;
+    place-content: center;
+    justify-items: center;
+    min-height: 100vh;
+    margin: 0;
+    padding: 1rem;
+    line-height: 1.5;
+    font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir,
+        helvetica neue, helvetica, Ubuntu, roboto, noto, segoe ui, arial, sans-serif;
+    color: #444;
+    background-color: #e1faf1;
+}
+
+.card h3 {
+    margin-top: 1rem;
+    font-size: 1.25rem;
+}
+
+.card a {
+    color: inherit;
+}
+
+.card-wrapper {
+    list-style: none;
+    padding: 0;
+    margin: 100px auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(30ch, 1fr));
+    grid-gap: 1.5rem;
+    max-width: 100vw;
+    width: 120ch;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
+
+.card-title {
     color: $orange;
 }
-</style>
+
+.card-text {
+    color: white;
+}
+
+.btn {
+    --bs-btn-border-color: orange;
+
+    &:hover {
+        --bs-btn-hover-bg: #ffc107;
+        --bs-btn-hover-border-color: #fc8019;
+        --bs-btn-active-bg: #ffc107;
+        --bs-btn-active-border-color: #fc8019;
+    }
+}
+
+.ingredient {
+    color: #EBB825;
+}</style>
